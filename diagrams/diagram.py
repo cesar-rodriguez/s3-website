1a4bf7d25c5cf2985a5c51dd19e78abf2095c475
Cesar Rodriguez <cesar@accurics.com>
2020-09-13 00:31:09 -0400

    adds compute diagrams

--- diagrams/diagram.py
+++ diagrams/diagram.py
@@ -2,7 +2,8 @@
 from diagrams import Cluster, Diagram
 from diagrams.aws.general import Users
 from diagrams.aws.storage import S3
-from diagrams.aws.network import CF
+from diagrams.aws.network import CF, ELB
+from diagrams.aws.compute import EC2
 
 with Diagram("Terrascan Website", show=False, filename="01-s3-public-bucket"):
     users = Users("users")
@@ -24,3 +25,28 @@ with Diagram("Terrascan Website", show=False, filename="03-s3-behind-cloudfront"
         s3 = S3("static-assets")
 
     users >> cf >> s3
+
+with Diagram("Terrascan Website", show=False, filename="04-public-elb-behind-cloudfront"):
+    users = Users("users")
+    with Cluster("Internet Exposed"):
+        cf = CF("CDN")
+        elb = ELB("Load Balancer")
+    with Cluster("Private Only"):
+        s3 = S3("static-assets")
+        ec2 = EC2("app-server")
+
+    users >> cf >> s3
+    cf >> elb >> ec2
+
+with Diagram("Terrascan Website", show=False, filename="05-lockdown-elb-behind-cloudfront"):
+    users = Users("users")
+    with Cluster("Internet Exposed"):
+        cf = CF("CDN")
+    with Cluster("CloudFront Only"):
+        elb = ELB("Load Balancer")
+    with Cluster("Private Only"):
+        s3 = S3("static-assets")
+        ec2 = EC2("app-server")
+
+    users >> cf >> s3
+    cf >> elb >> ec2
