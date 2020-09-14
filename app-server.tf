8b70c18cf567e99f0fcc1ab32a61dd937b0beb20
Cesar Rodriguez <cesar@accurics.com>
2020-09-14 00:34:15 -0400

    removes internet from sg

--- app-server.tf
+++ app-server.tf
@@ -24,8 +24,7 @@ resource "aws_security_group" "web" {
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = [
-      "10.0.0.0/16",
-      "0.0.0.0/0"
+      "10.0.0.0/16"
     ]
   }
   egress {
